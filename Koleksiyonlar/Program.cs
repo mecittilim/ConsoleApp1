ffe931cd7214b37a72664a4f5dc596223c6ec95b
Mecit Tilim <mecittilim@gmail.com>
2021-12-03 01:15:33 +0300

    2.21.21 Ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,16 @@
+﻿//string[] İsimler = new string[] { "engin", "ed4", "meco", "oko" };
+//Console.WriteLine(İsimler[0]);
+//Console.WriteLine(İsimler[1]);
+//Console.WriteLine(İsimler[2]);
+//Console.WriteLine(İsimler[3]);
+
+List<string> isimler2 = new List<string>() {"engin", "ed4", "meco", "oko"};
+Console.WriteLine(isimler2[0]);
+Console.WriteLine(isimler2[1]);
+Console.WriteLine(isimler2[2]);
+Console.WriteLine(isimler2[3]);
+isimler2.Add("ilker");
+Console.WriteLine(isimler2[4]);
+
+
+
---
+++ OOP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOP1;
+
+Product product1 = new Product();
+product1.Id = 1;
+product1.CategoryId = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnitsInStock = 3;
+
+Product product2 = new Product { Id = 2, CategoryId = 5, UnitssInStock = 5, ProductName = "kalem", UnitPrice = 35 };
+
+
+// PascalCase  //camelCase
+//case sensitive
+ProductManager productManager = new ProductManager();
+productManager.Add(product1);
+Console.WriteLine(product1);
+
+
+//intidouble,bool... değer tip
+//diziler,classes,abstract class, interface... referans tip
+// ref out
+
